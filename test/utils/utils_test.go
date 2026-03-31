a6a54b8d9299cfce7ded2611f1f28278a00c1d27
Bowen Sun <nusnewob@users.noreply.github.com>
2026-03-31 10:46:23 +0100

    fix: fix conflicts and other upgrade issues

--- test/utils/utils_test.go
+++ test/utils/utils_test.go
@@ -321,7 +321,7 @@ func TestConstantValues(t *testing.T) {
 		{
 			name:     "certmanager version",
 			value:    certmanagerVersion,
-			expected: "v1.19.4",
+			expected: "v1.20.0",
 		},
 		{
 			name:     "default kind binary",
@@ -345,7 +345,8 @@ func TestConstantValues(t *testing.T) {
 }
 
 func TestCertManagerURLTemplate(t *testing.T) {
-	expectedURL := "https://github.com/cert-manager/cert-manager/releases/download/v1.19.4/cert-manager.yaml"
+	expectedURL := "https://github.com/cert-manager/cert-manager/releases/download/" +
+		certmanagerVersion + "/cert-manager.yaml"
 
 	// Simulate URL construction from template
 	actualURL := "https://github.com/cert-manager/cert-manager/releases/download/" +
